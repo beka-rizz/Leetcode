ae26a4b9385badedffd5dd7e57ff22295689b213
bekarys_nurgasym <bekarysnurgassym@gmail.com>
2025-07-09 22:50:53 +0500

    solve problem #0069

---
+++ problems/easy/0069.sqrt(x).go
@@ -0,0 +1,16 @@
+package problems
+
+func MySqrt(x int) int {
+	if x == 0 || x == 1 {
+		return x
+	}
+
+	for i := 0; i <= x; i++ {
+		if i*i > x {
+			return i - 1
+		} else if i*i == x {
+			return i
+		}
+	}
+	return -1
+}
