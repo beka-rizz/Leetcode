3657167d72f1d95ed121bf9af0e13a8141d4ac67
bekarys_nurgasym <bekarysnurgassym@gmail.com>
2025-07-09 18:55:12 +0500

    solve problem #0009

--- problems/easy/0009.palindrome_num.go
+++ problems/easy/0009.palindrome_num.go
@@ -3,13 +3,13 @@ package problems
 import "fmt"
 
 func IsPalindrome(x int) bool {
-    if x < 0 {
+	if x < 0 {
 		return false
 	} else {
 		numString := fmt.Sprint(x)
 		for i, el := range numString {
 			mirror_el := numString[len(numString)-1-i]
-			if  mirror_el != byte(el) {
+			if mirror_el != byte(el) {
 				return false
 			}
 		}
