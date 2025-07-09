763cdbc6e55ab2cc48e0357330190c2a5c2c9c87
bekarys_nurgasym <bekarysnurgassym@gmail.com>
2025-07-09 19:18:51 +0500

    solve problem #0009 var-2

---
+++ problems/easy/0009.palindrome_num2.go
@@ -0,0 +1,22 @@
+package problems
+
+import (
+	"fmt"
+)
+
+func IsPalindrome2(x int) bool {
+	if x < 0 {
+		return false
+	} else if x < 10 {
+		return true
+	} else {
+		var reversedString string
+		var originalNumber = fmt.Sprint(x)
+		for x > 0 {
+			remainder := x % 10
+			reversedString += fmt.Sprint(remainder)
+			x /= 10
+		}
+		return originalNumber == reversedString
+	}
+}
