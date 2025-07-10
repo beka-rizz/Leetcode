48ebadccf00d19a42566b1444243eca26e8275af
bekarys_nurgasym <bekarysnurgassym@gmail.com>
2025-07-10 16:54:48 +0500

    solve problem #1512

---
+++ problems/easy/1512.num_good_pairs.go
@@ -0,0 +1,13 @@
+package easy
+
+func NumIdenticalPairs(nums []int) int {
+    res := 0
+    for i:=0;i<len(nums)-1;i++{
+        for j:=i+1;j<len(nums);j++{
+            if (nums[i]==nums[j]) {
+                res++
+            }
+        }
+    }
+    return res
+}
