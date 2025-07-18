5bbfa4bb587d23a52b52709da3b9b6f729a4cc22
bekarys_nurgasym <bekarysnurgassym@gmail.com>
2025-07-18 12:21:28 +0500

    solve problem #0169

---
+++ problems/easy/0169.majority_element.go
@@ -0,0 +1,12 @@
+package easy
+
+func MajorityElement(nums []int) int {
+    mp := make(map[int]int)
+    for _, num := range nums {
+        mp[num]++
+        if mp[num] > len(nums) / 2 {
+            return num
+        }
+    }
+    return -1
+}
